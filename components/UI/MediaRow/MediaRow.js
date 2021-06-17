993f8880e4e48fc27082e62f50e9198903334dce
Emmanuel Tolani <75217826+EmmanuelTolani@users.noreply.github.com>
2021-06-17 12:01:38 -0600

    Upgraded skeleton and added lazy load feature to my components. Added preloader to document

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -33,18 +33,18 @@ const MediaRow = (props) => {
     }
     return thumbnails;
   };
-  const showThumbnails = () => {
+  const showThumbnails = (type) => {
     return loadingData
       ? loopComp(<Skeleton />, 10)
       : movies.map((movie) => {
-          return <Thumbnail movieData={movie} />;
+          return <Thumbnail movieData={movie} type={type} />;
         });
   };
   return (
     <div className={`media-row ${props.type}`}>
       <h3 className="media-row__title">{props.title}</h3>
       <div className="media-row__thumbnails">
-        {showThumbnails()}
+        {showThumbnails(props.type)}
         {/* {loopComp(<Thumbnail />, 10)} */}
       </div>
     </div>
@@ -52,10 +52,26 @@ const MediaRow = (props) => {
 };
 
 const Thumbnail = (props) => {
+  const thumbSize = (type) => {
+    if (props.type === "large-v") {
+      return "400";
+    }
+    if (type === "small-v") {
+      return "185";
+    }
+    if (type === "large-h") {
+      return "500";
+    }
+    if (type === "small-h") {
+      return "342";
+    }
+  };
   return (
     <div className="media-row__thumbnail">
       <img
-        src={`https://image.tmdb.org/t/p/original${props.movieData.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w${thumbSize(props.type)}${
+          props.movieData.poster_path
+        }`}
         alt=""
       />
       <div className="media-row__top-layer">
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import MainLayout from "../components/Layouts/MainLayout";
 import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
 import MediaRow from "../components/UI/MediaRow/MediaRow";
 import AuthCheck from "../components/AuthCheck";
+import LazyLoad from "react-lazyload";
+import Placeholders from "../components/UI/PlaceHolders/Placeholders";
 export default function Home() {
   const globalState = useStateContext();
   const router = useRouter();
@@ -14,41 +16,76 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow
-        title="Popular Movies"
-        type="large-v"
-        endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Series"
-        type="small-h"
-        endpoint="discover/tv?sort_by=popularity.desc&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Action"
-        type="small-v"
-        endpoint="discover/movie?with_genres=28&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Horror"
-        type="large-v"
-        endpoint="discover/movie?with_genres=27&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Cartoons & Anime "
-        type="large-v"
-        endpoint="discover/movie?with_genres=16&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Romance"
-        type="large-h"
-        endpoint="discover/movie?with_genres=10749&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Sci-fi"
-        type="small-v"
-        endpoint="discover/movie?with_genres=878&primary_release_year=2021"
-      />
+      <LazyLoad
+        offset={-680}
+        placeholder={<Placeholders title="Popular Movies" type="large-v" />}
+      >
+        <MediaRow
+          title="Popular Movies"
+          type="large-v"
+          endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholders title="New Series" type="small-h" />}
+      >
+        <MediaRow
+          title="New Series"
+          type="large-h"
+          endpoint="discover/tv?primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholders title="Action" type="small-v" />}
+      >
+        <MediaRow
+          title="Action"
+          type="small-v"
+          endpoint="discover/movie?with_genres=28&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholders title="Horror" type="large-v" />}
+      >
+        <MediaRow
+          title="Horror"
+          type="small-v"
+          endpoint="discover/movie?with_genres=27&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholders title="Cartoons & Anime " type="large-v" />}
+      >
+        <MediaRow
+          title="Cartoons & Anime "
+          type="large-v"
+          endpoint="discover/movie?with_genres=16&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholders title="Romance" type="large-h" />}
+      >
+        <MediaRow
+          title="Romance"
+          type="large-v"
+          endpoint="discover/movie?with_genres=10749&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad
+        offset={-400}
+        placeholder={<Placeholders title="Sci-fi" type="small-v" />}
+      >
+        <MediaRow
+          title="Sci-fi"
+          type="small-v"
+          endpoint="discover/movie?with_genres=878&primary_release_year=2021"
+        />
+      </LazyLoad>
     </MainLayout>
   );
 }
