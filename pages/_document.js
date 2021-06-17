f8be26db071ff221b8ae3665f88e43f13057165d
Emmanuel Tolani <75217826+EmmanuelTolani@users.noreply.github.com>
2021-06-17 12:47:49 -0600

    added local font awesome

--- pages/_document.js
+++ pages/_document.js
@@ -16,14 +16,7 @@ class MyDocument extends Document {
             as="style"
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
           />
-          <link
-            rel="preload"
-            as="style"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-            integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
+          <link rel="stylesheet" href="/fontaw/css/all.min.css" />
         </Head>
         <body>
           <Main />
